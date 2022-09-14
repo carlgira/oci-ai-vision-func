ab9b23480818727b2a4c046209477cf8667e38f0
carlgira@gmail.com <carlgira@gmail.com>
2022-09-14 15:58:54 +0200

    Default response

--- func.py
+++ func.py
@@ -32,23 +32,19 @@ def handler(ctx, data: io.BytesIO = None):
 
         if r["labels"] is not None and len(r["labels"]) > 0:
             label_id = r["labels"][0]["name"]
-            logging.getLogger().info('label_id in labels: ' + label_id)
             r["details"] = get_item_details(label_id)
 
         if r["image_objects"] is not None and len(r["image_objects"]) > 0:
             label_id = r["image_objects"][0]["name"]
-            logging.getLogger().info('label_id in image_objects: ' + label_id)
             r["details"] = get_item_details(label_id)
 
         return response.Response(ctx, response_data=json.dumps(r), headers={"Content-Type": "application/json"})
 
     except (Exception, ValueError) as ex:
-        logging.getLogger().info('error parsing json payload: ' + str(ex))
-
-    logging.getLogger().info("Inside Python Hello World function")
+        logging.getLogger().info('Error with vision service call: ' + str(ex))
 
     return response.Response(
-        ctx, response_data=json.dumps({'problem': 502}),
+        ctx, response_data=json.dumps({'An error ocurred': 502}),
         headers={"Content-Type": "application/json"}
     )
 
@@ -77,6 +73,9 @@ def analize_image(image_encoded, model_id=None):
 # Database functions
 
 def get_text_secret(secret_ocid):
+    '''
+    Function to get the secrets from the vault
+    '''
     try:
         secret_content = secret_client.get_secret_bundle(secret_ocid).data.secret_bundle_content.content.encode('utf-8')
         decrypted_secret_content = base64.b64decode(secret_content).decode("utf-8")
@@ -86,9 +85,11 @@ def get_text_secret(secret_ocid):
         raise
 
 def get_item_details(item_id):
+    '''
+    Get an item from the databse using an item_id
+    '''
     con = None
     try:
-        logging.getLogger().info("data " + username + ' ' + ' ' + password + ' ' + db_url + ' ' + os.environ["TNS_ADMIN"])
         con = cx_Oracle.connect(username, password, db_url)
         with con.cursor() as cursor:
             for row in cursor.execute("SELECT c.doc FROM ITEM_DETAILS c where c.doc.id = '%s'" % item_id):
@@ -100,7 +101,7 @@ def get_item_details(item_id):
         if con is not None:
             con.close()
 
-    return {"id" : item_id, "name" : "NO_MATCH"}
+    return {"id" : item_id, "name" : item_id}
 
 username = os.environ["ATP_USERNAME"]
 password = get_text_secret(os.environ["PASSWORD_SECRET_OCID"])
